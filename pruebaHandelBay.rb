011920be7595f205b7baa67b1789ed4bf0b2a307
SebasTamayoA <sebastamayo37@gmail.com>
2022-09-19 08:40:21 -0500

    Ejercicio 5

--- pruebaHandelBay.rb
+++ pruebaHandelBay.rb
@@ -8,4 +8,24 @@ def multiplicar(numero1, numero2)
   resultado
 end
 
+puts 'Ejercicio 1'
 puts multiplicar(3,5)
+puts
+
+# Ejercicio 5
+def cantidad_palabras(frase)
+  cantidad = {} # uso de hash para mostrar las cantidades de palabras de una frase
+  palabras = frase.split(' ')
+  palabras.each do |palabra|
+    if cantidad[palabra]
+      cantidad[palabra] += 1
+    else
+      cantidad[palabra] = 1
+    end
+  end
+  cantidad
+end
+
+puts 'Ejercicio 5'
+puts cantidad_palabras('Prueba de Handel bay Prueba bay')
+puts
